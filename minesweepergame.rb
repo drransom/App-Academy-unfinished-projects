a2d88be8507bdb1053a40021ec88a6526b93b3ae
Elliot Reed <elliotreed2@gmail.com>
2015-02-23 13:52:37 -0800

    board class and intialize

--- minesweepergame.rb
+++ minesweepergame.rb
@@ -3,6 +3,36 @@ class MinesweeperGame
 end
 
 class Board
+  attr_reader :tiles
+  def initialize(num_bombs = 9)
+    @num_bombs = num_bombs
+    create_tiles #sets @tiles variable
+  end
+
+  def create_tiles
+    bomb_tile_numbers = select_bombs
+    @tiles = []
+    BOARD_SIZE.times do |idx1|
+      BOARD_SIZE.times do |idx2|
+        bomb = bomb_tile_numbers.include?(BOARD_SIZE*idx1 + idx2)
+        @tiles << Tile.new([idx1, idx2], bomb)
+      end
+    end
+  end
+
+  def select_bombs
+    (0...(BOARD_SIZE**2)).to_a.sample(@num_bombs)
+  end
+
+
+
+  def [](xcord, ycord)
+    @tiles.select do |tile|
+      tile.position == [xcord, ycord]
+    end[0]
+  end
+
+
 
 end
 
@@ -11,7 +41,7 @@ class Tile
   def self.tiles
     @@tiles
   end
-  attr_reader :bomb, :display
+  attr_reader :bomb, :display, :position
   NEIGHBOR_PERMS = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1],
                     [1, 0], [1, 1]]
 
@@ -23,7 +53,6 @@ class Tile
     @neighbors = []
     @revealed = false
     @@tiles[@position] = self
-    @@count += 1
   end
 
   def reveal
