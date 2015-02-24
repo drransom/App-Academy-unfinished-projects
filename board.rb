92348945d5cd2168e225e15c160d088259af0a8f
Elliot Reed <elliotreed2@gmail.com>
2015-02-23 17:26:47 -0800

    refactor neighbor logic from tile to board

--- board.rb
+++ board.rb
@@ -1,4 +1,7 @@
 class Board
+  NEIGHBOR_PERMS = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1],
+                    [1, 0], [1, 1]]
+
   attr_reader :tiles
 
   def initialize(num_bombs = 9)
@@ -8,11 +11,11 @@ class Board
 
   def create_tiles
     bomb_tile_numbers = select_bombs
-    @tiles = []
+    @tiles = {}
     BOARD_SIZE.times do |idx1|
       BOARD_SIZE.times do |idx2|
         bomb = bomb_tile_numbers.include?(BOARD_SIZE * idx1 + idx2)
-        @tiles << Tile.new([idx1, idx2], bomb)
+        @tiles[[idx1, idx2]] = Tile.new([idx1, idx2], self, bomb)
       end
     end
   end
@@ -22,33 +25,50 @@ class Board
   end
 
   def [](position)
-    @tiles.find { |tile| tile.position == [position[0], position[1]] }
-    # @tiles.select do |tile|
-    #   tile.position == [position[0], position[1]]
-    # end[0]
+    @tiles[[position[0], position[1]]]
   end
 
   def create_display
     display_string = ""
     BOARD_SIZE.times do |idx1|
       BOARD_SIZE.times do |idx2|
-        display_string += self[[idx1,idx2]].display + ' '
+        display_string += @tiles[[idx1,idx2]].display + ' '
       end
       display_string += "\n"
     end
+
     display_string.strip
   end
 
   def bomb_revealed?
-    @tiles.any? { |tile| tile.bomb? && tile.revealed? }
+    @tiles.any? { |_key, tile| tile.bomb? && tile.revealed? }
   end
 
   def won?
-    revealed = @tiles.count { |tile| tile.revealed? && !tile.bomb? }
+    revealed = @tiles.each_value { |tile| tile.revealed? && !tile.bomb? }
     revealed == (BOARD_SIZE ** 2) - @num_bombs
   end
 
   def reveal_board
-    @tiles.each { |tile| tile.reveal if tile.bomb? }
+    @tiles.each_value { |tile| tile.reveal if tile.bomb? }
+  end
+
+  def valid_position?(arr)
+    arr.all? {|value| value.between?(0, BOARD_SIZE-1)}
+  end
+
+  def calculate_position(tile, perm)
+    [tile.position[0] + perm[0], tile.position[1] + perm[1]]
+  end
+
+  def find_neighbors_of_tile(tile)
+    neighbors = []
+    NEIGHBOR_PERMS.each do |perm|
+      possible_position = calculate_position(tile, perm)
+      neighbors << @tiles[possible_position] if
+                    valid_position?(possible_position)
+    end
+
+    neighbors
   end
 end
--- tile.rb
+++ tile.rb
@@ -1,21 +1,13 @@
 class Tile
-  # TA: tiles hash stored in the board?
-  @@tiles = {}
-  def self.tiles
-    @@tiles
-  end
   attr_reader :display, :position, :neighbors
-  NEIGHBOR_PERMS = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1],
-                    [1, 0], [1, 1]]
 
-  def initialize(position, bomb = false)
-    @explored = false
+  def initialize(position, board, bomb = false)
     @bomb = bomb
     @display = '*'
+    @board = board
     @position = position
     @neighbors = []
     @revealed = false
-    @@tiles[@position] = self
   end
 
   def reveal
@@ -30,19 +22,7 @@ class Tile
   end
 
   def find_neighbors
-    NEIGHBOR_PERMS.each do |perm|
-      possible_position = calculate_position(perm)
-      @neighbors << @@tiles[possible_position] if
-                    valid_position?(possible_position)
-    end
-  end
-
-  def valid_position?(arr)
-    arr.all? {|value| value.between?(0, BOARD_SIZE-1)}
-  end
-
-  def calculate_position(perm)
-    [@position[0] + perm[0], @position[1] + perm[1]]
+    @neighbors = @board.find_neighbors_of_tile(self)
   end
 
   def toggle_flag
