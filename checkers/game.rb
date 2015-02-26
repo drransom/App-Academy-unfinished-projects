e979636758e2d323250aeac96d5394055f38c625
Elliot Reed <elliotreed2@gmail.com>
2015-02-26 14:16:46 -0800

    add basic structure to game object.

--- checkers/game.rb
+++ checkers/game.rb
@@ -3,4 +3,29 @@ require_relative 'piece'
 require_relative 'checkers_helper'
 
 class CheckersGame
+  def initialize(options = {})
+    default = {capture_required: true, size: 8}
+    options = default.merge(options)
+    @capture_required = options[:capture_required]
+    @size = options[:size]
+    @board = CheckersBoard.new(options)
+  end
+
+  def play_game
+    initialize_game
+    run_game
+    display_result
+  end
+
+  def initialize_game
+    puts "Welcome to checkers, the first nontrivial game " +
+         "that was perfectly solved!"
+  end
+
+  def run_game
+  end
+
+  def display_result
+  end
+
 end
