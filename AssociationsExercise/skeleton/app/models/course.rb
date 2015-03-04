6871e2087cdac69cc9c8a6d6cf19ebaecc27ea89
Elliot Reed <elliotreed2@gmail.com>
2015-03-04 13:33:14 -0800

    add associations

--- AssociationsExercise/skeleton/app/models/course.rb
+++ AssociationsExercise/skeleton/app/models/course.rb
@@ -1,2 +1,14 @@
 class Course < ActiveRecord::Base
+  has_many(
+    :enrollments,
+    class_name: 'Enrollment',
+    foreign_key: :course_id,
+    primary_key: :id
+  )
+
+  has_many(
+    :enrolled_students,
+    through: :enrollments,
+    source: :student
+  )
 end
