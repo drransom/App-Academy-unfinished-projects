795b1f03b1cb22f04a967db8c2c274e47b31eca4
Elliot Reed <elliotreed2@gmail.com>
2015-03-04 14:56:48 -0800

    add submitter and submitted_url associations

--- UrlShortener/app/models/shortened_url.rb
+++ UrlShortener/app/models/shortened_url.rb
@@ -2,6 +2,13 @@ class ShortenedUrl < ActiveRecord::Base
   validates :submitter_id, :short_url, :long_url, presence: true
   validates :short_url, uniqueness: true
 
+  belongs_to(
+    :submitter,
+    class_name: 'User',
+    primary_key: :id,
+    foreign_key: :submitter_id
+  )
+
   def self.create_for_user_and_long_url!(user, long_url)
     short_url = ShortenedUrl.random_code
     ShortenedUrl.create!(submitter_id: user.id, long_url: long_url, short_url: short_url)
