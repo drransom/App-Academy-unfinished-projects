cfc30a1b124db401ede0f2826c8ceaca2c0d3cbd
Elliot Reed <elliotreed2@gmail.com>
2015-03-04 15:25:05 -0800

    added num_clicks method for ShortenedUrl

--- UrlShortener/app/models/shortened_url.rb
+++ UrlShortener/app/models/shortened_url.rb
@@ -9,6 +9,19 @@ class ShortenedUrl < ActiveRecord::Base
     foreign_key: :submitter_id
   )
 
+  has_many(
+    :visits,
+    class_name: 'Visit',
+    primary_key: :id,
+    foreign_key: :url_id
+  )
+
+  has_many(
+    :visitors,
+    through: :visits,
+    source: :user
+  )
+
   def self.create_for_user_and_long_url!(user, long_url)
     short_url = ShortenedUrl.random_code
     ShortenedUrl.create!(submitter_id: user.id, long_url: long_url, short_url: short_url)
@@ -19,5 +32,9 @@ class ShortenedUrl < ActiveRecord::Base
       random_url = SecureRandom.urlsafe_base64
       return random_url unless ShortenedUrl.exists?(short_url: random_url)
     end
+  end
+
+  def num_clicks
+    Visit.where("url_id = ?", id).count
   end
 end
--- UrlShortener/app/models/user.rb
+++ UrlShortener/app/models/user.rb
@@ -7,4 +7,18 @@ class User < ActiveRecord::Base
     primary_key: :id,
     foreign_key: :submitter_id
   )
+
+  has_many(
+    :visits,
+    class_name: 'Visit',
+    primary_key: :id,
+    foreign_key: :user_id
+  )
+
+  has_many(
+    :visited_urls,
+    through: :visits,
+    source: :url
+  )
+
 end
