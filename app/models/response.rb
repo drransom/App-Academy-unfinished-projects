3b427c9dd9395c9263a97a84110c808a2d9d4729
App Academy Student <student@appacademy.io>
2015-03-05 14:52:50 -0800

    Add custom validation for responses from same user

--- app/models/response.rb
+++ app/models/response.rb
@@ -11,6 +11,7 @@
 
 class Response < ActiveRecord::Base
   validates :user_id, :answer_choice_id, presence: true
+  validate :user_has_not_already_answered_question
 
   belongs_to(
     :respondent,
@@ -25,4 +26,16 @@ class Response < ActiveRecord::Base
     primary_key: :id,
     foreign_key: :answer_choice_id
   )
+
+  has_one(:question, through: :answer_choice, source: :question)
+
+  def sibling_responses
+    question.responses.where(":id IS NULL OR responses.id != :id", id: id)
+  end
+
+  def user_has_not_already_answered_question
+    if sibling_responses.where("responses.user_id != ?", user_id).count > 0
+      errors[:user] << "has already answered this question."
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+appacademy = User.create(user_name: 'appacademy')
+hackreactor = User.create(user_name: 'hackreactor')
+hackbright = User.create(user_name: 'hackbright')
+
+poll1 = Poll.create(title: 'aa_poll', author_id: appacademy.id)
+poll2 = Poll.create(title: 'hb_poll', author_id: hackbright.id)
+
+question1 = Question.create(poll_id: poll1.id, text: 'Who is the fairest of them all?')
+question2 = Question.create(poll_id: poll1.id, text: 'Can I hold it back any more')
+question3 = Question.create(poll_id: poll2.id, text: 'Who am I?')
+
+answer1 = AnswerChoice.create(question_id: question1.id, text: 'Harry Potter')
+answer2 = AnswerChoice.create(question_id: question1.id, text: 'Dumbledore')
+answer3 = AnswerChoice.create(question_id: question2.id, text: 'Yes!')
+answer4 = AnswerChoice.create(question_id: question2.id, text: 'Absolutely not.')
+
+answer5 = AnswerChoice.create(question_id: question3.id, text: '24601')
+answer6 = AnswerChoice.create(question_id: question3.id, text: 'Jean Valjean')
+
+Response.create!(respondent: appacademy, answer_choice: answer6)
+Response.create!(respondent: hackbright, answer_choice: answer2)
+Response.create!(respondent: hackreactor, answer_choice: answer1)
