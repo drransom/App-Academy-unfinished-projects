e45d1f7ab129b0b994e49f84258729e99f20088b
Elliot Reed <elliotreed2@gmail.com>
2015-03-04 18:16:26 -0800

    tried to get TagTopic#most_popular_links to work

--- UrlShortener/app/models/tag_topic.rb
+++ UrlShortener/app/models/tag_topic.rb
@@ -11,4 +11,29 @@ class TagTopic < ActiveRecord::Base
     through: :taggings,
     source: :shortened_url
   )
+
+# TO DO: get this shit to work, maybe
+  # def most_popular_links(n = 1)
+  #   ShortenedUrl.select(:long_url).where('taggings.tag_topic_id = ?', id).group(shortened_urls.id).order((:long_url).count DESC)
+  #
+  #
+  #
+  #
+  #   # db.execute(<<-SQL, id, n)
+  #   #   SELECT
+  #   #     *
+  #   #   FROM
+  #   #     shortened_urls
+  #   #   JOIN
+  #   #     taggings ON shortened_urls.id = taggings.shortened_url_id
+  #   #   WHERE
+  #   #     taggings.tag_topic_id = ?
+  #   #   GROUP BY
+  #   #     shortened_urls.id
+  #   #   ORDER BY
+  #   #     COUNT(*) DESC
+  #   #   LIMIT
+  #   #     ?
+  #   # SQL
+  # end
 end
