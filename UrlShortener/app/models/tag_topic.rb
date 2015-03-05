1fa78c321ed0066c1e55b6ba5ffd67d27cce1563
Elliot Reed <elliotreed2@gmail.com>
2015-03-04 20:43:54 -0800

    add most popular links

--- UrlShortener/app/models/tag_topic.rb
+++ UrlShortener/app/models/tag_topic.rb
@@ -12,28 +12,14 @@ class TagTopic < ActiveRecord::Base
     source: :shortened_url
   )
 
-# TO DO: get this shit to work, maybe
-  # def most_popular_links(n = 1)
-  #   ShortenedUrl.select(:long_url).where('taggings.tag_topic_id = ?', id).group(shortened_urls.id).order((:long_url).count DESC)
-  #
-  #
-  #
-  #
-  #   # db.execute(<<-SQL, id, n)
-  #   #   SELECT
-  #   #     *
-  #   #   FROM
-  #   #     shortened_urls
-  #   #   JOIN
-  #   #     taggings ON shortened_urls.id = taggings.shortened_url_id
-  #   #   WHERE
-  #   #     taggings.tag_topic_id = ?
-  #   #   GROUP BY
-  #   #     shortened_urls.id
-  #   #   ORDER BY
-  #   #     COUNT(*) DESC
-  #   #   LIMIT
-  #   #     ?
-  #   # SQL
-  # end
+  def most_popular_links(n = 1)
+    ShortenedUrl
+      .select("shortened_urls.*")
+      .joins(:taggings)
+      .select("taggings.*, count(shortened_urls.id) as scount")
+      .where("taggings.tag_topic_id = ?", id)
+      .group("shortened_urls.id")
+      .order("scount DESC")
+      .limit(n)
+  end
 end
