1a2670d319fd37d2dcdc4577d8826ff79e809f85
Dan Dzoan Elliot Reed <dandzoan@gmail.com>
2015-03-16 17:41:36 -0700

    write specs for comments

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -2,12 +2,13 @@
 #
 # Table name: goals
 #
-#  id         :integer          not null, primary key
-#  content    :text             not null
-#  user_id    :integer          not null
-#  private    :boolean          not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id           :integer          not null, primary key
+#  content      :text             not null
+#  user_id      :integer          not null
+#  private      :boolean          not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  completed_on :date
 #
 
 class Goal < ActiveRecord::Base
